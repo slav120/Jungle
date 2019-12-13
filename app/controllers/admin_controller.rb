4d8e50533ff4947474c96229de91110c8c7a567a
Slav Lalev <slalev57@me.com>
2019-12-13 01:40:15 +0000

    added user authentication and admin categories

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,6 @@
+
+class AdminController < ApplicationController
+def categories
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,15 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
+
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
